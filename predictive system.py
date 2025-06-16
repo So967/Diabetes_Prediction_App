e7bd6a44aa2b977f451e33d71d2ee4b94aaae8e9
Soham Baidya <baidyasalil@gmail.com>
2025-06-16 18:19:27 +0530

    Update predictive system.py

--- predictive system.py
+++ predictive system.py
@@ -9,7 +9,7 @@ import numpy as np
 import pickle
 
 # loading the saved model
-loaded_model=pickle.load(open('C:/Users/USER/OneDrive/Desktop/Machile Learning project/trained_model.sav','rb'))
+loaded_model=pickle.load(open('C:/Users/USER/OneDrive/Desktop/Machine Learning project/trained_model.sav','rb'))
 
 
 input_data=(5,166,72,19,175,25.8,0.587,51)
