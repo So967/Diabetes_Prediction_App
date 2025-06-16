fecee13acd4c79084e759d4ad8fade763b795dd9
Soham Baidya <baidyasalil@gmail.com>
2025-06-16 18:19:50 +0530

    Update Diabetes Prediction web app.py

--- Diabetes Prediction web app.py
+++ Diabetes Prediction web app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model=pickle.load(open('C:/Users/USER/OneDrive/Desktop/Machile Learning project/trained_model.sav','rb'))
+loaded_model=pickle.load(open('C:/Users/USER/OneDrive/Desktop/Machine Learning project/trained_model.sav','rb'))
 
 
 # creating a function for Prediction
